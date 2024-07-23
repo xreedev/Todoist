960c5404009ea9d1ac9130bb799ba314e7a0056e
sreedev <xreedev@gmail.com>
2024-07-23 14:38:55 +0530

    markasdoe func added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+taskCount=0;
 function addTask(){
     const inputTasc=document.getElementById("task").value;
     createTask(inputTasc);
 }
 
 function createTask(inputTasc){
+    taskCount+=1;
     let taskList=document.getElementById("tasks-list");
     let newItem=document.createElement("li")
     let doneButton=document.createElement("button");
@@ -11,6 +13,27 @@ function createTask(inputTasc){
     newItem.classList.add("todo-task");
     doneButton.textContent="✅";
     doneButton.classList.add("done-button");
+    doneButton.addEventListener("click",function(){markAsDone(this.id)});
+    newItem.id="task-"+taskCount;
+    doneButton.id=taskCount;
     newItem.appendChild(doneButton);
     taskList.appendChild(newItem);
 }
+
+function markAsDone(id){
+   this.addEventListener("click",function(){
+    markAsNotDone(this.id)
+   });
+   let doneItem=document.getElementById("task-"+id);
+   doneItem.classList.remove("todo-task");
+   doneItem.classList.add("completed-task");
+}
+
+function markAsNotDone(id){
+    this.addEventListener("click",function(){
+        markAsDone(this.id)
+    });
+    let doneItem=document.getElementById("task-"+id);
+    doneItem.classList.remove("completed-task");
+    doneItem.classList.add("todo-task");
+ }

66614b1192903800692e9382b606898da28957a1
sreedev <xreedev@gmail.com>
2024-07-23 14:05:22 +0530

    todo list logic complete

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function addTask(){
-    let inputTasc=document.getElementById("task").textContent;
+    const inputTasc=document.getElementById("task").value;
+    createTask(inputTasc);
+}
+
+function createTask(inputTasc){
+    let taskList=document.getElementById("tasks-list");
+    let newItem=document.createElement("li")
+    let doneButton=document.createElement("button");
+    newItem.textContent=inputTasc;
+    newItem.classList.add("todo-task");
+    doneButton.textContent="✅";
+    doneButton.classList.add("done-button");
+    newItem.appendChild(doneButton);
+    taskList.appendChild(newItem);
 }
